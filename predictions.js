ad456369f7d899bc57c381ebf660575fa1688218
Serge Chvorun <elifnull@protonmail.com>
2022-02-09 00:11:18 -0500

    predictions 1-5

--- predictions.js
+++ predictions.js
@@ -16,10 +16,10 @@ const employee = {
 }
 const { name: otherName } = employee;
 //Predict the output
-console.log(name);
+console.log(name); //produces a reference error at runtime
 console.log(otherName);
 
-//Prediction2:
+//Prediction2: null or undefined Refference error, "Elon"
 
 //Problem 3:
 const person = {
@@ -31,9 +31,9 @@ const password = '12345';
 const { password: hashedPassword } = person;  
 //Predict the output
 console.log(password);
-console.log(hashedPassword);
+console.log(hashedPassword); //typeError: of undefined or null
 
-//Prediction3:
+//Prediction3: "12345" and typeError of undefined
 
 //Problem 4:
 const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
@@ -41,10 +41,10 @@ const [,first] = numbers;
 const [,,,second] = numbers;
 const [,,,,,,,,third] = numbers;
 //Predict the output
-console.log(first == second);
-console.log(first == third);
+console.log(first == second); //this is a comparison, nets a bool
+console.log(first == third); //This is a comparison, nets a bool
 
-//Prediction4:
+//Prediction4: False, True
 
 
 //Problem 5:
@@ -52,13 +52,13 @@ const lastTest = {
     key: 'value',
     secondKey: [1, 5, 1, 8, 3, 3]
 }
-const { key } = lastTest;
-const { secondKey } = lastTest;
-const [ ,willThisWork] = secondKey;
+const { key } = lastTest; //'value'
+const { secondKey } = lastTest; //whole array
+const [ ,willThisWork] = secondKey; // no this will not work...I was wrong!
 //Predict the output
 console.log(key);
 console.log(secondKey);
 console.log(secondKey[0]);
 console.log(willThisWork);
 
-//Prediction 5:
+//Prediction 5: 'value', [1,5,1,8,3,3], 1, undefined
