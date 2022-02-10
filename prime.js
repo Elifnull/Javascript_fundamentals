e0b2930b0fd78193d2f386a13397279fcc290e3e
Serge Chvorun <elifnull@protonmail.com>
2022-02-10 18:35:59 -0500

    prime number update

--- prime.js
+++ prime.js
@@ -50,6 +50,11 @@ console.log(iFib(20));
 console.log(`This took ${performance.now() - start} milliseconds to run`); //iterative method runs faster: 3.83 milisec vs 4.9 milisec
 
 
-const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
+const { performance } = require('perf_hooks');
+const start = performance.now();
 
+const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
+console.log(story.length)
 const reversed1 = story.split("").reverse().join("");
+console.log(reversed1)
+console.log(`This took ${performance.now() - start} milliseconds to run`)
