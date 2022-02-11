07f5e29c1470cfdef42b7cf26c2c3013c3ec6bd5
Serge Chvorun <elifnull@protonmail.com>
2022-02-11 16:04:55 -0500

    array & object methods test

--- practice.js
+++ practice.js
@@ -18,19 +18,29 @@
 // }
 // printNames(beatles);
 
-class Deck {
-    constructor() {
-        const suits = ['Diamond', 'Heart', 'Spade', 'Club'];
-        const faces = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
-        const deck = [];
-        for (const suit of suits) {
-        for (const face of faces) {
-            deck.push(this.createCard(suit, face));
-        }
-        }
-        this.deck = deck;
-    }
-    createCard(suit, face) {
-        return face + " of " + suit;
-    }
-}
+// class Deck {
+//     constructor() {
+//         const suits = ['Diamond', 'Heart', 'Spade', 'Club'];
+//         const faces = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+//         const deck = [];
+//         for (const suit of suits) {
+//         for (const face of faces) {
+//             deck.push(this.createCard(suit, face));
+//         }
+//         }
+//         this.deck = deck;
+//     }
+//     createCard(suit, face) {
+//         return face + " of " + suit;
+//     }
+// }
+
+const exampleFunction = (message) => console.log( message );
+
+exampleFunction('Testing out Arrow Functions, Callback functions!');
+
+isEven = number => number % 2 === 0 ;
+
+arrayTest = [2,2,4,8,6].every(isEven)
+
+console.log(arrayTest)
