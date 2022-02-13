bdfd62badbdb5afe81a1c6eae54d0ee2ff648a39
Serge Chvorun <elifnull@protonmail.com>
2022-02-12 19:53:08 -0500

    bubble sort fix

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,3 +1,17 @@
 //Bubble sort data
 
-test = [1, 2, 6, 10, 0, -1, 5]
+const test = [1,9,12,]
+
+const bubbleSort = array => {
+    for(let i =0; i < array.length; i++) {
+        for(let d = 0; d < array.length - i; d++) {
+            if(array[d] > array[d+1]) {
+                let temp = array[d];
+                array[d] = array[d+1];
+                array[d+1] = temp;
+            }
+        }
+    }return array
+}
+
+console.log(bubbleSort(test));
